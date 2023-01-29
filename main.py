c2135402110b0bcd3d0e1baa72e5ed5be5735e49
dylon-wetzel <122506667+dylon-wetzel@users.noreply.github.com>
2023-01-29 10:46:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Hello world!')
+print("Hello world!")
+print("Hello " + "world!")
+print("Hello" + " world!")
+
+print("Hello", end="\n")
+print("world!")
+
+print("Hello", end=" ")
+print("world!")
+
+print("Hello", "world!", "it is the end of the week")
+
+name = input("Enter your name: ")
